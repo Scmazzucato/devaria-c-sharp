9e9dbd530f2607b1f9ec2c675fcbd6d2df44b5de
Samuel Mazzucato <89219911+Scmazzucato@users.noreply.github.com>
2021-09-13 15:34:56 -0300

    Aula C#-2

---
+++ Estruturas-de-repetição/Estruturas-de-repetição/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Estruturas_de_repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length == 0)
+            {
+                Console.WriteLine("Favor informar palavras no argumento!");
+                return;
+            }
+            for (var indice = 0;  indice < args.Length; indice++)
+            {
+                Console.WriteLine($"Estrutura for, indice e valor: {indice},{args[indice]}");
+            }
+            var argumentosLidos = 0;
+            while(argumentosLidos < args.Length)
+            {
+                Console.WriteLine($"Estrutura while, argumentos lidos e valor lidos: {argumentosLidos}, {args[argumentosLidos]}");
+                argumentosLidos++;
+            }
+            var loopsEfetuados = 0;
+            do
+            {
+                Console.WriteLine($"Estrutura do while, loops efetuados: {loopsEfetuados} e valor lido: {args[loopsEfetuados]}");
+                loopsEfetuados++;
+            } while (loopsEfetuados < args.Length);
+            foreach(var arg in args)
+            {
+                Console.WriteLine($"Estrutura foreach valor lido: {arg}");
+            }
+        }
+    }
+}
---
+++ Lista-de-compras/Lista-de-compras/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Lista_de_compras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> produtosDisponiveis = new List<string>()
+            {
+                "Pão", "Leite", "Manteiga", "Carne", "Café", "Frango", "Batata", "Chocolate", "Arroz", "Feijão"
+            };
+            try
+            {
+                var produtosSelecionados = produtosDisponiveis.Where(produto => args.Contains(produto)).ToList();   
+                foreach (var produtoSelecionado in produtosSelecionados)
+                {
+                    Console.WriteLine($"Este produto nos temos: {produtoSelecionado}");
+                }
+                var produtosNaoDisponiveis = args.Where(args => !produtosDisponiveis.Contains(args)).ToList();
+                foreach(var produtosNaoDisponivel in produtosNaoDisponiveis)
+                {
+                    Console.WriteLine($"Este produto nos não temos infelizmente=/ : {produtosNaoDisponivel}");
+                }
+                var produtosDisponiveisOrdenadoPorNome = produtosDisponiveis.OrderBy(e => e).ToList();
+                foreach(var produtorOrdenados in produtosDisponiveisOrdenadoPorNome)
+                {
+                    Console.WriteLine($"Segue este produto disponível: {produtorOrdenados}");
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Argumentos Inválidos");
+            }
+        }
+    }
+}
