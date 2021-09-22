56aedccd4a61869e884b8b17e185e3a947642524
Samuel Mazzucato <89219911+Scmazzucato@users.noreply.github.com>
2021-09-22 15:51:10 -0300

    Minha Primeira Api

---
+++ Minha-primeira-API/Minha-primeira-API/Controllers/Usuario-Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Minha_primeira_API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Minha_primeira_API.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")]
+    public class Usuario_Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult ObterUsuario()
+        {
+            var usuario = new Usuario()
+            {
+                Nome = "Teste",
+                Email = "Teste@teste.com",
+                Senha = "12345",
+            };
+            return Ok(usuario);
+        }
+    }
+}
